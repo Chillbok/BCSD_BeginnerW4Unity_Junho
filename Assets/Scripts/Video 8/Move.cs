246b456581646b7b3eccb816726d9432c4edb2ee
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-04-08 00:50:27 +0900

    video8 실습 완료

--- Assets/Scripts/Video 8/Move.cs
+++ Assets/Scripts/Video 8/Move.cs
@@ -2,40 +2,56 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    Vector2 target = new Vector2(10,5.5f);
-    Vector3 target3 = new Vector3(10,5.5f,1);
+    Vector3 target = new Vector3(10,1,10);
     // Update is called once per frame
+    int point = 4;
     void Update()
     {
-        //1. MoveTowards(단순 등속 이동)
-        /*
-        transform.position =
-            Vector2.MoveTowards(transform.position, target, 0.05f);//x좌표, y좌표, 움직이는 속도
-        
-        //2. SmoothDamp(미끄러지듯이 감속 이동)
-        Vector2 velo = Vector2.zero;
-
-        //마지막 매개변수에 반비례해서 속도 증가
-        //값을 많이 줄수록 속도 빨라짐
-        transform.position =
-            Vector2.SmoothDamp(transform.position,target,ref velo, 0.1f);
-
-        //3. Lerf (선형 보간 이동)
-        //값을 적게 줄수록 속도 빨라짐
-        transform.position =
-            Vector2.Lerp(transform.position,
-            target,
-            0.01f);
-        */
-
-        //4. Slerp (구면 선형 보간 이동)
-        //포물선 이동
-        //vec2(2D)에서는 사용 불가능
-        transform.position =
-            Vector3.Slerp(
-                transform,
-                target3,
-                0.05f
+        Vector3 velo = Vector3.zero;
+        if(point == 1)
+        {
+            //1. MoveTowards(단순 등속 이동)
+            Debug.Log("MoveTowards 실행");
+            transform.position = Vector3.MoveTowards(
+                transform.position, //현재 위치 벡터
+                target,             //목표 위치 벡터
+                1f                  //한 프레임 동안 이동할 수 있는 최대 거리
+                );
+            Debug.Log("MoveTowards 실행 완료");
+        }
+        else if(point == 2)
+        {
+            //2. SmoothDamp(미끄러지듯이 감속 이동)
+            Debug.Log("SmoothDamp 실행");
+            transform.position = Vector3.SmoothDamp(
+                transform.position, //현재 벡터 값
+                target,             //목표 벡터 값(도달하고자 하는 최종 위치)
+                ref velo,           //현재 속도 젝터를 저장하는 변수. Vector3.zero로 초기화하는 것이 일반적.
+                0.1f                //목표 값에 도달하는 데 대략적으로 걸리는 시간(초). 값이 작을수록 더 빠르게 목표에 도달.
+                );
+            Debug.Log("SmoothDamp 실행 완료");
+        }
+        else if(point == 3)
+        {
+            //3. Lerp(선형 보간)
+            Debug.Log("Lerp 실행");
+            transform.position = Vector3.Lerp(
+                    transform.position, //시작 벡터
+                    target,             //목표 벡터
+                    0.1f                //보간 비율. 0.0f ~ 1.0f 사이의 float 값을 가짐.
+                );
+            Debug.Log("Lerp 실행 완료");
+        }
+        else if(point == 4)
+        {
+            //4. Slerp(구형 선형 보간)
+            Debug.Log("Slerp 실행");
+            transform.position = Vector3.Slerp(
+                transform.position, //시작 벡터
+                target,             //목표 벡터
+                0.005f              //보간 비율. 0.0f ~ 1.0f 사이의 float 값을 가짐.
             );
+            Debug.Log("Slerp 실행 완료");
+        }
     }
 }
