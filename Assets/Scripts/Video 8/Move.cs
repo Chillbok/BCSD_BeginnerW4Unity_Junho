e3a1891f063f212b22a581ad3b1a1a310858f0cf
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-04-07 19:43:41 +0900

    video8 실습 진행중

--- Assets/Scripts/Video 8/Move.cs
+++ Assets/Scripts/Video 8/Move.cs
@@ -11,17 +11,14 @@ public class Move : MonoBehaviour
         /*
         transform.position =
             Vector2.MoveTowards(transform.position, target, 0.05f);//x좌표, y좌표, 움직이는 속도
-        */
         
         //2. SmoothDamp(미끄러지듯이 감속 이동)
         Vector2 velo = Vector2.zero;
 
         //마지막 매개변수에 반비례해서 속도 증가
         //값을 많이 줄수록 속도 빨라짐
-        /*
         transform.position =
             Vector2.SmoothDamp(transform.position,target,ref velo, 0.1f);
-        */
 
         //3. Lerf (선형 보간 이동)
         //값을 적게 줄수록 속도 빨라짐
@@ -29,17 +26,16 @@ public class Move : MonoBehaviour
             Vector2.Lerp(transform.position,
             target,
             0.01f);
+        */
 
         //4. Slerp (구면 선형 보간 이동)
         //포물선 이동
         //vec2(2D)에서는 사용 불가능
-        /*
         transform.position =
             Vector3.Slerp(
                 transform,
                 target3,
                 0.05f
             );
-        */
     }
 }
